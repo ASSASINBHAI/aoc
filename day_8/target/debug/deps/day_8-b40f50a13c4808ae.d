2ab9ef4113ffd4ab67c3cb2699744820cdfcf5aa
ASSASSINBHAI <tusherg742@gmail.com>
2024-12-09 16:53:37 +0530

    remove day_8 folder from the repository

--- day_8/target/debug/deps/day_8-b40f50a13c4808ae.d
+++
@@ -1,5 +0,0 @@
-D:\rust projects\AOC 2024\day_8\target\debug\deps\libday_8-b40f50a13c4808ae.rmeta: src/main.rs
-
-D:\rust projects\AOC 2024\day_8\target\debug\deps\day_8-b40f50a13c4808ae.d: src/main.rs
-
-src/main.rs:
