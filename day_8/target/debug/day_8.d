e4141992d1225bc0a45677b290f88e04382b1b3f
ASSASSINBHAI <tusherg742@gmail.com>
2024-12-09 16:51:48 +0530

    Remove day_8 folder from the repository

--- day_8/target/debug/day_8.d
+++
@@ -1,1 +0,0 @@
-D:\rust\ projects\AOC\ 2024\day_8\target\debug\day_8.exe: D:\rust\ projects\AOC\ 2024\day_8\src\main.rs
--- day_8/target/debug/deps/day_8.d
+++
@@ -1,5 +0,0 @@
-D:\rust projects\AOC 2024\day_8\target\debug\deps\day_8.exe: src/main.rs
-
-D:\rust projects\AOC 2024\day_8\target\debug\deps\day_8.d: src/main.rs
-
-src/main.rs:
